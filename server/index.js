ffb66a0618f7625785dee14bfe16a03396a6a699
sapSap7 <sapir280@gmail.com>
2025-07-03 14:30:38 +0300

    server

--- server/index.js
+++ server/index.js
@@ -17,6 +17,8 @@ app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
+console.log("MONGO_URI is:", process.env.MONGO_URI);
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
