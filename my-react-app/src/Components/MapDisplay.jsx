8e2018df7d369fdb19a320450a88472b2a4f42c9
sapSap7 <sapir280@gmail.com>
2025-07-16 11:37:43 +0300

    map

--- my-react-app/src/Components/MapDisplay.jsx
+++ my-react-app/src/Components/MapDisplay.jsx
@@ -1,5 +1,20 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import { useEffect, useState } from "react";
+
+function FlyToLocation({ coordinates }) {
+  const map = useMap();
+
+  useEffect(() => {
+    if (coordinates) {
+      map.flyTo([coordinates.lat, coordinates.lon], 13, {
+        animate: true,
+        duration: 1.5,
+      });
+    }
+  }, [coordinates, map]);
+
+  return null;
+}
 
 export default function MapDisplay({ coordinates }) {
   const [position, setPosition] = useState([32.0853, 34.7818]); // Default: Tel Aviv
@@ -17,6 +32,7 @@ export default function MapDisplay({ coordinates }) {
       style={{ height: "300px", width: "100%" }}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <FlyToLocation coordinates={coordinates} />
       <Marker position={position}>
         <Popup>Your location</Popup>
       </Marker>
